bdf48f44476174e3a8737521ccab859814b9500f
NnNeEediIMm <83602085+NnNeEediIMm@users.noreply.github.com>
2021-09-30 14:45:39 +0200

    Use this with rb movement and my camera controller

--- WallRun.cs
+++ WallRun.cs
@@ -12,6 +12,9 @@ public class WallRun : MonoBehaviour
     public float rotateAngle = 4.5f; 
     int defaultGravity;
 
+    bool jumping;
+    KeyCode jumpInput;
+
     RaycastHit hit;
 
     private void Start()
@@ -20,21 +23,35 @@ public class WallRun : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         cmare = playerCamera.GetComponent<cameraMove>();
         defaultGravity = movement.gravityScale;
+        jumpInput = movement.jump;
     }
 
     private void Update()
     {
+        jumping = Input.GetKeyDown(jumpInput);
+        float magnitudeJ = 90f;
+
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.right), out hit, 1f, wall))
         {
             movement.gravityScale = 0;
             rb.useGravity = false;
             cmare.eulerAngle = rotateAngle;
+            if (jumping)
+            {
+                transform.Translate(Vector3.left * 0.1f);
+                rb.AddForce(Vector3.up * movement.jumpForce * magnitudeJ);
+            }
         }
         else if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.left), out hit, 1f, wall))
         {
             movement.gravityScale = 0;
             rb.useGravity = false;
             cmare.eulerAngle = -rotateAngle;
+            if (jumping)
+            {
+                transform.Translate(Vector3.right * 0.1f);
+                rb.AddForce(Vector3.up * movement.jumpForce * magnitudeJ);
+            }
         }
         else
         {
